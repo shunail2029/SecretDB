4b09788b7e2ba7c52b352a793ec09fce00fdb7be
shunail2029 <tiger1998nov27@gmail.com>
2020-12-04 18:12:38 +0900

    fix startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 secretdbd unsafe-reset-all
+
+rm $HOME/.secretdbd/config/genesis.json
+rm $HOME/.secretdbd/config/gentx/*
+rm  $HOME/.secretdbcli/config/config.toml
+
 secretdbd init mynode --chain-id test1
 
-secretdbcli config keyring-backend test1
+secretdbcli config keyring-backend test
 secretdbcli config chain-id test1
 secretdbcli config output json
 secretdbcli config indent true
@@ -16,6 +21,6 @@ secretdbd add-genesis-account $(secretdbcli keys show user1 -a) 1000token,100000
 secretdbd add-genesis-account $(secretdbcli keys show user2 -a) 1000token
 
 secretdbd gentx --name user1 --keyring-backend test
-secretdbd collect-gentx
+secretdbd collect-gentxs
 
 secretdbd start
